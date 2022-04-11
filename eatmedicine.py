396c56ca95893e9b45fce84f69147682af494611
HyunBin Yu <edward0103.yu@gmail.com>
2022-04-11 23:04:36 +0900

    update and bugfix

--- eatmedicine.py
+++ eatmedicine.py
@@ -36,8 +36,6 @@ async def on_ready():
             
             lastclock = clock
         await asyncio.sleep(60)
-        
-                     
 
 @bot.event
 async def on_message(message):
@@ -45,6 +43,10 @@ async def on_message(message):
         return None
     await bot.process_commands(message)
 
+@bot.command("꺼")
+async def logout(ctx):
+    if ctx.author.id == "739698524115173387":
+        quit()
 
 @bot.command("먹을래")
 async def react_test(ctx):
@@ -60,21 +62,18 @@ async def react_test(ctx):
     if not 0 <= int(mes.content) <= 23:
         await ctx.send("아니요. 그렇게 입력하면 안돼요. 처음부터 다시 해주세요.")
     else:
-        await ctx.send("오늘부터 매일 " + mes.content + "시 정각에 DM으로 알려줄게요.")
-        #print(type(ctx.author))
-        timelist[int(mes.content)].append(ctx.author.id)
-        with open("timelist.txt", "w", encoding="UTF-8") as f:
-            f.write(str(timelist))
-
-        print(timelist)    
-
-    return None
-
+        if not ctx.author.id in timelist:
+            await ctx.send("오늘부터 매일 " + mes.content + "시 정각에 DM으로 알려줄게요.")
+            timelist[int(mes.content)].append(ctx.author.id)
+            with open("timelist.txt", "w", encoding="UTF-8") as f:
+                f.write(str(timelist))
+        else:
+            await ctx.send("이미" + mes.content + "시에 알림을 받아요.")
+    
 @bot.command("안먹을래")
 async def anmuke(ctx):
     def check(mes: Message):
         return mes.author == ctx.author and mes.channel == ctx.channel
-
     try:
         await ctx.send(str(ctx.author) + "님, 몇 시에 안 드실래요?")
         mes: Message = await ctx.bot.wait_for('message', check=check, timeout=30)
@@ -97,7 +96,7 @@ async def help(ctx):
 @bot.command("언제먹어")
 async def when(ctx):
     wheneat = []
-    for i in range(23):
+    for i in range(24):
         if ctx.author.id in timelist[i]:
             wheneat.append(i)
     if not str(wheneat) == "[]":
